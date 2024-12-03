53a77f6c385435901d42ba9f0d6441a97bc9b71d
janEbert <janpublicebert@posteo.net>
2024-12-03 14:18:43 +0100

    Support gradient accumulation

--- torchtitan/train.py
+++ torchtitan/train.py
@@ -121,6 +121,27 @@ class Trainer(torch.distributed.checkpoint.stateful.Stateful):
         )
         self.train_spec = train_spec_module.get_train_spec(job_config.model.name)
 
+        # verify batch sizes
+        if job_config.training.global_batch_size is None:
+            job_config.training.global_batch_size = \
+                job_config.training.batch_size * dp_degree
+        assert job_config.training.global_batch_size > 0
+        assert (
+            job_config.training.global_batch_size
+            % (job_config.training.batch_size * dp_degree)
+            == 0
+        ), (
+            f"global batch size must be multiple of local batch size times "
+            f"data-parallel degree ({job_config.training.global_batch_size} "
+            f"% ({job_config.training.batch_size} * {dp_degree}) != 0)"
+        )
+
+        self.gradient_accumulation_steps = (
+            job_config.training.global_batch_size
+            // (job_config.training.batch_size * dp_degree)
+        )
+        assert self.gradient_accumulation_steps > 0
+
         # build dataloader
         tokenizer = self.train_spec.tokenizer_cls(job_config.model.tokenizer_path)
 
@@ -302,7 +323,8 @@ class Trainer(torch.distributed.checkpoint.stateful.Stateful):
         logger.info(
             f"Training starts at step {self.step + 1}, "
             f"with local batch size {job_config.training.batch_size}, "
-            f"global batch size {job_config.training.batch_size * dp_degree}, "
+            f"global batch size {job_config.training.global_batch_size}, "
+            f"gradient accumulation steps {self.gradient_accumulation_steps}, "
             f"sequence length {job_config.training.seq_len}, "
             f"total steps {job_config.training.steps} "
             f"(warmup {job_config.lr_scheduler.warmup_steps})"
@@ -371,7 +393,7 @@ class Trainer(torch.distributed.checkpoint.stateful.Stateful):
                 loss.backward()
         return loss
 
-    def train_step(self, inputs: torch.Tensor, labels: torch.Tensor):
+    def train_step(self):
         self.optimizers.zero_grad()
 
         # Keep these variables local to shorten the code as these are
@@ -380,7 +402,10 @@ class Trainer(torch.distributed.checkpoint.stateful.Stateful):
         world_mesh = self.world_mesh
         parallel_dims = self.parallel_dims
 
-        loss = self.batch_backward(inputs, labels)
+        for microbatch in range(self.gradient_accumulation_steps):
+            inputs, labels = self.next_batch(data_iterator)
+            loss = self.batch_backward(inputs, labels)
+            self.metrics_processor.accumulated_losses.append(loss.detach())
 
         dist_utils.clip_grad_norm_(
             [p for m in model_parts for p in m.parameters()],
@@ -391,6 +416,9 @@ class Trainer(torch.distributed.checkpoint.stateful.Stateful):
         self.checkpointer.maybe_wait_for_staging()
         self.optimizers.step()
         self.lr_schedulers.step()
+
+        loss = torch.mean(torch.stack(self.metrics_processor.accumulated_losses))
+        self.metrics_processor.accumulated_losses.clear()
 
         # log metrics
         if not self.metrics_processor.should_log(self.step):
@@ -424,8 +452,7 @@ class Trainer(torch.distributed.checkpoint.stateful.Stateful):
             while self.step < job_config.training.steps:
                 self.step += 1
                 self.gc_handler.run(self.step)
-                inputs, labels = self.next_batch(data_iterator)
-                self.train_step(inputs, labels)
+                self.train_step()
                 self.checkpointer.save(
                     self.step, force=(self.step == job_config.training.steps)
                 )
