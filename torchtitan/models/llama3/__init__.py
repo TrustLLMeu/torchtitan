b7e854a88e24aff2062c6e87376ef7543899ea18
janEbert <janpublicebert@posteo.net>
2025-02-25 17:23:19 +0100

    Add Byte tokenizer spec

--- torchtitan/models/llama3/__init__.py
+++ torchtitan/models/llama3/__init__.py
@@ -15,7 +15,7 @@ from torchtitan.protocols.train_spec import TrainSpec
 
 from .infra.parallelize import parallelize_llama
 from .model.args import TransformerModelArgs
-from .model.model import Transformer
+from .model.model import BitNetTransformer, Transformer
 from .model.state_dict_adapter import Llama3StateDictAdapter
 
 __all__ = [
