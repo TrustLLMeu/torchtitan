2ffddd0d483550552a2964418e70508cad4d539c
Oleg Filatov <ofivite@users.noreply.github.com>
2025-04-08 13:48:13 +0200

    Allow using non-parametric RMSNorm

--- torchtitan/models/norms.py
+++ torchtitan/models/norms.py
@@ -13,7 +13,7 @@ def build_norm(norm_type: str, dim: int, eps: float = 1e-6):
 
     Args:
         norm_type (str): The type of normalization layer to build.
-            Supported types: layernorm, np_layernorm, rmsnorm
+            Supported types: layernorm, np_layernorm, rmsnorm, np_rmsnorm
         dim (int): The dimension of the normalization layer.
         eps (float, optional): The epsilon value for numerical stability. Defaults to 1e-6.
 
@@ -31,5 +31,7 @@ def build_norm(norm_type: str, dim: int, eps: float = 1e-6):
         return nn.LayerNorm(dim, eps=eps, elementwise_affine=False, bias=False)
     elif norm_type == "rmsnorm":
         return nn.RMSNorm(dim, eps=eps)
+    elif norm_type == "np_rmsnorm":
+        return nn.RMSNorm(dim, eps=eps, elementwise_affine=False)
     else:
         raise NotImplementedError(f"Unknown norm_type: '{norm_type}'")
