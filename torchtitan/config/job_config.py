74b743494e6a2bec5d13e4b9e605557603de90b5
Oleg Filatov <ofivite@users.noreply.github.com>
2025-04-10 09:26:31 +0200

    Allow logging parameter and update norms

--- torchtitan/config/job_config.py
+++ torchtitan/config/job_config.py
@@ -46,6 +46,9 @@ class Metrics:
     log_freq: int = 10
     """How often to log metrics to TensorBoard, in iterations"""
 
+    log_norm_freq: int = 0
+    """How often to log parameter norm metrics to TensorBoard, in iterations"""
+
     enable_tensorboard: bool = False
     """Whether to log metrics to TensorBoard"""
 
