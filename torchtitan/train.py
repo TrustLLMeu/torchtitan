78ffcca8ee0fc8b58ae0b8ba297db4b3c8c83ec5
Oleg Filatov <ofivite@users.noreply.github.com>
2025-04-10 18:34:39 +0200

    Allow disabling gradient clipping

--- torchtitan/train.py
+++ torchtitan/train.py
@@ -504,12 +504,14 @@ class Trainer(torch.distributed.checkpoint.stateful.Stateful):
                         else:
                             raise Exception(f"p_name: {p_name} and p.grad is None")
 
-        grad_norm = dist_utils.clip_grad_norm_(
-            [p for m in model_parts for p in m.parameters()],
-            self.job_config.training.max_norm,
-            foreach=True,
-            pp_mesh=self.world_mesh["pp"] if parallel_dims.pp_enabled else None,
-        )
+        grad_norm = None
+        if self.job_config.training.max_norm > 0:
+            grad_norm = dist_utils.clip_grad_norm_(
+                [p for m in model_parts for p in m.parameters()],
+                self.job_config.training.max_norm,
+                foreach=True,
+                pp_mesh=self.world_mesh["pp"] if parallel_dims.pp_enabled else None,
+            )
         self.checkpointer.maybe_wait_for_staging()
         self.optimizers.step()
         self.lr_schedulers.step()
@@ -555,9 +557,9 @@ class Trainer(torch.distributed.checkpoint.stateful.Stateful):
         else:
             global_avg_loss = global_max_loss = loss.detach().item()
 
-        extra_metrics = {
-            "optim/grad_norm": grad_norm,
-        }
+        extra_metrics = {}
+        if grad_norm is not None:
+            extra_metrics["optim/grad_norm"] = grad_norm
         extra_metrics.update(self.optimizers.get_lrs())
         if (
                 self.job_config.metrics.log_norm_freq > 0
@@ -573,10 +575,11 @@ class Trainer(torch.distributed.checkpoint.stateful.Stateful):
                 extra_metrics[f"loss_metrics/moe_entropy_per_layer_{k}"] = v
 
         color = self.metrics_processor.color
-        extra_print_data = (
-            f"  {color.green}gradnorm: {grad_norm:7.4f}{color.reset}"
-        )
-
+        extra_print_data = ""
+        if grad_norm is not None:
+            extra_print_data = (
+                f"{extra_print_data}  {color.green}gradnorm: {grad_norm:7.4f}{color.reset}"
+            )
         self.metrics_processor.log(
             self.step, global_avg_loss, global_max_loss, extra_metrics, extra_print_data,
         )
