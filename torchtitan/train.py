6565bc0c5b750096511739e50b463a8c7e298102
rakkit <rakkit@users.noreply.github.com>
2025-04-16 15:44:01 +0200

    Fix expert parallelism

--- torchtitan/train.py
+++ torchtitan/train.py
@@ -476,14 +476,16 @@ class Trainer(torch.distributed.checkpoint.stateful.Stateful):
             if hasattr(model, "update_gate_bias"):
                 model.update_gate_bias()
 
-        # # TODO(JSC): if we shard the MoE model, we need to remove the following code
+        # TODO(JSC): Scaled the EP's grad by 1/EP, we still need to scale expert's grad by 1/EP
+        # maybe we can move this to Expert Parallel as a grad hook[?] to make the
         for model in model_parts:
-            for p_name, p in model.named_parameters():
-                if p.grad is None:
-                    raise Exception(f"p_name: {p_name} and p.grad is None")
+            for p_name, param in model.named_parameters():
+                if param.requires_grad and ".experts" in p_name:
+                    param.grad = param.grad / parallel_dims.ep
 
-        grad_norm = None
         # TODO(JSC): disable gradient clipping for now for debugging
+        # Adamw + EP seems does not work with gradient clipping
+        grad_norm = None
         if self.job_config.training.max_norm > 0:
             grad_norm = dist_utils.clip_grad_norm_(
                 [p for m in model_parts for p in m.parameters()],
