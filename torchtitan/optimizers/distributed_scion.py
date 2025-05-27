d0fc4f1fea6a2a2834d59f5099f9f19121fd676e
rakkit <rakkit@users.noreply.github.com>
2025-05-27 10:56:10 +0200

    Update Scion implementation capabilities
    We previously errored out on some settings, but this is no longer
    required.

--- torchtitan/optimizers/distributed_scion.py
+++ torchtitan/optimizers/distributed_scion.py
@@ -143,6 +143,12 @@ class DistributedScion(torch.optim.Optimizer):
             for param in group["params"]:
                 self.paramters_to_groups[id(param)] = group_idx
 
+            if self.is_light and nesterov:
+                raise RuntimeError(
+                    "Nesterov momentum is not supported for Scion's light mode. "
+                    "Please set nesterov=False."
+                )
+
     @torch.no_grad()
     def step(self, closure=None):
         loss = None
@@ -289,7 +295,6 @@ class DistributedScion(torch.optim.Optimizer):
                 p.data.add_(u, alpha=-lr)
 
             if momentum != 1 and self.is_light:
-                raise NotImplementedError("Scion-light is not ready yet")
                 p.grad.mul_(1 - momentum)
 
     def step_sgd(self, sgd_params):
@@ -315,7 +320,6 @@ class DistributedScion(torch.optim.Optimizer):
                 p.data.add_(u, alpha=-lr)
 
             if momentum != 1 and self.is_light:
-                raise NotImplementedError("Scion-light is not ready yet")
                 g.mul_(1 - momentum)
 
     def step_ddp(self, ddp_params):
@@ -492,7 +496,6 @@ class DistributedScion(torch.optim.Optimizer):
             p.data.to_local().add_(u, alpha=-lr)
 
             if momentum != 1 and self.is_light:
-                raise NotImplementedError("Scion-light is not ready yet")
                 g.mul_(1 - momentum)
 
     @torch.no_grad()
