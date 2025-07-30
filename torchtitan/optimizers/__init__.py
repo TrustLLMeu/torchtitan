49e713cd2549400ea137320f8bf392e3131fc843
janEbert <janpublicebert@posteo.net>
2025-07-30 14:34:55 +0200

    Refactor module name clean-up function

--- torchtitan/optimizers/__init__.py
+++ torchtitan/optimizers/__init__.py
@@ -1,2 +1,3 @@
 from .distributed_scion import DistributedScion
 from .scion import Scion
+from .utils import remove_orig_mod_and_weight_for_p_name
---
+++ torchtitan/optimizers/utils.py
@@ -0,0 +1,12 @@
+import re
+
+
+def remove_orig_mod_and_weight_for_p_name(name: str) -> str:
+    """
+    Remove "._orig_mod", ".weight", and "._checkpoint_wrapped_module" to
+    get the clean layer name.
+    """
+    name = re.sub(r"\._orig_mod", "", name)  # comes from compiled model
+    name = re.sub(r"\.weight", "", name)  # param.weight
+    name = re.sub(r"\._checkpoint_wrapped_module", "", name)  # comes from activation checkpointing
+    return name
